37300ba781c24140d3a40b16650e77ea2ce79288
Jonathan Davies <jonnie@cleverna.me>
2025-10-22 15:40:38 +0100

    revert

--- crates/vizia_baseview/src/application.rs
+++ crates/vizia_baseview/src/application.rs
@@ -530,9 +530,9 @@ impl ApplicationRunner {
 
                     self.cx.needs_refresh(Entity::root());
 
-                    // Set scale factor to ONLY system DPI, not including user scale
-                    // User scale is handled by the physical window size change
-                    self.cx.set_scale_factor(self.window_scale_factor);
+                    self.cx.set_scale_factor(
+                        self.window_scale_factor * self.current_user_scale_factor,
+                    );
 
                     self.cx.send_event(Event::new(WindowEvent::WindowResized(WindowSize {
                         width: physical_size.0,
